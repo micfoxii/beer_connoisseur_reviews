3384c0cc6c80eeb159b32dc1fcd402b3753a5e00
Mic Fox <michael.c.fox.ii@gmail.com>
2019-07-13 17:06:44 -0400

    blocked out CLI and starting to reformat scraper (got lost in looking at student scraper and worlds best restaurants

--- lib/beer_reviews/beers.rb
+++ lib/beer_reviews/beers.rb
@@ -1,10 +1,24 @@
 class BeerReviews::Beers 
-    # attr_accessor :name, :styles, :brewery
-    # @@all = []
-
-    # def self.all
-    #     @@all
-    # end
-
+    attr_reader :name, :style, :brewery
     
+    @@all = []
+
+    def self.create_beers(b)
+        self.new(
+            b.css("b").text, 
+            b.css("#extendedInfo a")[0].text,
+            b.css("#extendedInfo a")[1].text
+        )
+    end
+
+    def initialize(name=nil, style=nil, brewery=nil)
+        @name = name
+        @style = style
+        @brewery = brewery
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- lib/beer_reviews/cli.rb
+++ lib/beer_reviews/cli.rb
@@ -1,43 +1,41 @@
 class BeerReviews::CLI
-    def call
-        BeerReviews::Scraper.scrape_beers
-       get_beer_name_brewery_type
-        list_beer_name_brewery_type
-        get_user_beer_selection
-    end
+    # def call
+    #     BeerReviews::Scraper.scrape_beers
+    #     puts "Are you ready to find your new favorite beer?"
+    #     start
+    # end
 
-    def get_beer_name_brewery_type
-        @beers = BeerReviews::Beers.all
-    end
+    # def get_beer_name_brewery_type
+    #     @beers = BeerReviews::Beers.all
+    # end
 
-    def list_beer_name_brewery_type
-        puts 'Welcome, are you ready to find your new favorite beer?'
-        @beers.each.with_index(1) do |beer, i|
-            puts "#{i}. #{beer.name} - #{beer.brewery} - #{beer.type}"
-        end
-    end
+    # def list_beer_name_brewery_type
+    #     BeerReviews::Beers.all do |beer, index|
+    #         puts "#{index}. #{beers.name} - #{beers.style} - #{beers.brewery}"
+    #     end
+    # end
 
-    def get_user_beer_selection
-        input = nil
-        while input != "exit"
-            puts "Please enter the number of the beer you would like to learn more about, or type exit to leave."
-            input = gets.strip.downcase
+    # def get_user_beer_selection
+    #     input = nil
+    #     while input != "exit"
+    #         puts "Please enter the number of the beer you would like to learn more about, or type exit to leave."
+    #         input = gets.strip.downcase
 
-            if input.to_i > 0 && input.to_i < @beers.length + 1
-                selected_beer = @beers[input.to_i-1]
-                puts " #{selected_beer.name} - #{selected_beer.brewery} - #{selected_beer.type}"
-            elsif input == "list"
-               list_beer_name_brewery_type
-            elsif input.strip.downcase == "exit"
-                 goodbye
-            else
-                puts "Sorry, I do not understand. Please type list to return to beer list, or type exit to leave."
-            end
-        end
-    end
+    #         if input.to_i > 0 && input.to_i < @beers.length + 1
+    #             selected_beer = @beers[input.to_i-1]
+    #             puts " #{selected_beer.name} - #{selected_beer.brewery} - #{selected_beer.type}"
+    #         elsif input == "list"
+    #            list_beer_name_brewery_type
+    #         elsif input.strip.downcase == "exit"
+    #              goodbye
+    #         else
+    #             puts "Sorry, I do not understand. Please type list to return to beer list, or type exit to leave."
+    #         end
+    #     end
+    # end
 
-    def goodbye
-        puts "See you next time and remember to drink responsibly!"
-    end
+    # def goodbye
+    #     puts "See you next time and remember to drink responsibly!"
+    # end
 
 end
